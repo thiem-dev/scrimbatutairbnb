1a2524db8962774e3df221faa2ee0a9cbf52c224
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-04 18:02:59 -0500

    passing itetm obj down

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,29 @@
 import React from "react"
 
 export default function Card(props) {
-// console.log(props)
+    const item = props.item;
+
+    let badgeText
+    if (item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (item.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+
 
     return (
         <div className="card">
-            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
-            <img src={`/images/${props.img}`} className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`/images/${item.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="/images/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{item.stats.rating}</span>
+                <span className="gray">({item.reviewCount}) • </span>
+                <span className="gray">{item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p>{item.title}</p>
+            <p><span className="bold">From ${item.price}</span> / person</p>
         </div>
     )
 }
