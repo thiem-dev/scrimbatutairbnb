e8842c788a312f2e2af33e07596497574807a1bb
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-04 17:50:37 -0500

    conditional rendering setup for badge

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
                 location={item.location}
                 title={item.title}
                 price={item.price}
+                openSpots={item.openSpots}
             />
         )
     })
