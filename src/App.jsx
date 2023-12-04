05e9b0ae727fe4a98d993a828b25f50170efdd30
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-03 19:30:44 -0500

    static page setup. need data mapping for component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react"
+import './App.css'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-import './App.css'
+import Card from "./components/Card"
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Card />
         </div>
     )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import katieImg from '../images/katie-zaferes.png';
+import star from '../images/star.png'
+
+export default function Card() {
+    return (
+        <div className="card">
+            <img src={katieImg} className="card--image" />
+            <div className="card--stats">
+                <img src={star} className="card--star" />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
+}
